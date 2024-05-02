7e3d243c4da147636645ae6155c162955a95b57f
Hamza Fatnaoui <hamzafatnaoui.ai@gmail.com>
2024-05-02 15:01:25 +0100

    Adding 2 models Student and Institute. Student has a foreinkey which institute. then i did makemigrations, after that migrate.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    GENDERS = (
+        ("f","female"),
+        ("m","male"),
+        ("u","undisclosed")
+    )
+    name = models.CharField(max_length=200)
+    roll_number = models.IntegerField(unique=True)
+    email = models.EmailField(100)
+    gender = models.CharField(max_length=1,choices=GENDERS)
+    percentage = models.FloatField()
+    institute = models.ForeignKey('Institute',on_delete=models.CASCADE ,null=True,blank=True)
+
+class Institute(models.Model):
+    TYPES = {
+        ("c","college"),
+        ("h","high cschool")
+    }
+    name = models.CharField(max_length=100)
+    type_of_institute = models.CharField(max_length=1,choices=TYPES)
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from api import views
-
-urlpatters=[
+urlpatterns = [
     
 ]
