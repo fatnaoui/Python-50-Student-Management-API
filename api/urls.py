17bbde271ee39bc49bb21ebf531bf291448496f0
Hamza Fatnaoui <hamzafatnaoui.ai@gmail.com>
2024-05-02 15:41:27 +0100

    Creating our first class view for listing the students by get request.

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from api import views
 urlpatterns = [
-    
+    path('students/',views.StudentAPI.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from api import models
+from api import serialisers
+from rest_framework.generics import (
+    ListAPIView,
+    ListCreateAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
 
 # Create your views here.
+class StudentAPI(ListAPIView):
+    queryset=models.Student.objects.all()
+    serializer_class=serialisers.StudentSerializer
