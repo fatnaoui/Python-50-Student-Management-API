6a815754a8acddd25f025beee425e3b2a871623f
Hamza Fatnaoui <hamzafatnaoui.ai@gmail.com>
2024-05-02 15:14:06 +0100

    Adding __srt__ function

--- api/models.py
+++ api/models.py
@@ -9,10 +9,12 @@ class Student(models.Model):
     )
     name = models.CharField(max_length=200)
     roll_number = models.IntegerField(unique=True)
-    email = models.EmailField(100)
+    email = models.EmailField(max_length=100)
     gender = models.CharField(max_length=1,choices=GENDERS)
     percentage = models.FloatField()
     institute = models.ForeignKey('Institute',on_delete=models.CASCADE ,null=True,blank=True)
+    def __str__(self):
+        return self.name
 
 class Institute(models.Model):
     TYPES = {
@@ -21,3 +23,5 @@ class Institute(models.Model):
     }
     name = models.CharField(max_length=100)
     type_of_institute = models.CharField(max_length=1,choices=TYPES)
+    def __str__(self):
+        return self.name
